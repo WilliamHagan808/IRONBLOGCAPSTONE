73b1776427537bd60945c047937ed3833b9deb2d
William Hagan <williamhagan1728@yahoo.com>
2022-05-15 19:34:33 -0700

    user endpoints working

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,6 +1,12 @@
 import  express from "express";
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import router from "./routes/user-routes";
 const app = express()
+
+app.use(express.json());
+app.use("/api/user",router);
+
+
 
 mongoose.connect('mongodb+srv://admin:aloha808@cluster0.juhn4.mongodb.net/IronBlogApp?retryWrites=true&w=majority')
 .then(()=>app.listen(5000)).then(()=>console.log("Connected and listening to Localhost 5000")).catch((err)=>console.log(err))
