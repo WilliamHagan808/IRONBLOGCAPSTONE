515636b5e0e00b7d47f3050e7f2ecccba951ed99
William Hagan <williamhagan1728@yahoo.com>
2022-05-15 18:35:19 -0700

    connected to mongodb

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,8 +1,6 @@
-import  Express, { request }  from "express";
-
+import  express from "express";
+import mongoose from 'mongoose'
 const app = express()
-app.use("/", (req, res, next) =>{
-    res.send("Hello World")
-})
 
-app.listen(5000)
+mongoose.connect('mongodb+srv://admin:aloha808@cluster0.juhn4.mongodb.net/IronBlogApp?retryWrites=true&w=majority')
+.then(()=>app.listen(5000)).then(()=>console.log("Connected and listening to Localhost 5000")).catch((err)=>console.log(err))
