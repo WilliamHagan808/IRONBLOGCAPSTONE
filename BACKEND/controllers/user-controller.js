78fb5b7274ef3df014c7dd167967b0fde8cbbc94
William Hagan <williamhagan1728@yahoo.com>
2022-05-18 13:06:19 -0700

    debug login issue in backend

--- BACKEND/controllers/user-controller.js
+++ BACKEND/controllers/user-controller.js
@@ -43,3 +43,23 @@ export const signup = async (req,res,next) => {
     return res.status(201).json({user});
 
 };
+
+export const login = async(req, res, next) => {
+    const {email, password} = req.body;
+    let  existingUser;
+    try {
+        existingUser = await User.findOne({email});
+    } catch (err) {
+      return  console.log(err);
+    }
+    if(!existingUser) {
+        return res.status(404)
+        .json({message: "Could not find user by this email."})
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password);
+    if(!isPasswordCorrect) {
+        return res.status(400).json({message:"Incorrect Password"})
+    }
+    return res.status(200).json({message:"Login Successfull"})
+}
