9b7bf47ec289d7a647c839d07d8ca01b7c38daef
William Hagan <williamhagan1728@yahoo.com>
2022-05-23 17:03:12 -0700

    added update past blog feature

--- BACKEND/routes/blog-routes.js
+++ BACKEND/routes/blog-routes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { getAllBlogs } from "../controllers/blog-controller";
+import { addBlog, getAllBlogs, updateBlog } from "../controllers/blog-controller";
 const blogRouter = express.Router();
 
 blogRouter.get("/", getAllBlogs);
+blogRouter.post("/add" , addBlog);
+blogRouter.put('/update/:id', updateBlog)
 
 export default blogRouter;
